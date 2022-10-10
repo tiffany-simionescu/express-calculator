84e4fc5f97fef1a3a4dc72e479d764ad393334dd
tiffany-simionescu <tsimionescu87@gmail.com>
2022-10-09 21:05:16 -0400

    Can now add 2 numbers using express.

--- calculator.js
+++ calculator.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+
+// Passing data from the html form with body-parser
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 // Root Directory
 app.get("/", function (req, res) {
     res.sendFile(`${__dirname}/index.html`);
+});
+
+// POST
+app.post("/", function (req, res) {
+    // Addition
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+
+    var result = num1 + num2;
+
+    res.send(`${num1} plus ${num2} is equal to ${result}`);
 });
 
 // Server Setup
